08cca754f3dca8027c18d36b8fed1319f5cd9770
eloycarneiro <40106015+eloycarneiro@users.noreply.github.com>
2023-09-24 16:32:54 -0300

    Create pessoa.js

---
+++ pessoa.js
@@ -0,0 +1,16 @@
+//Atributos de Pessoa
+
+let nome; //string
+let salario; //number
+let idade; //number
+let possuiDiploma //boolean
+
+nome = 'Clark'
+salario = 2052.90
+idade = 40
+possuiDiploma = true
+
+nome = 'Bruce'
+salario = 3934.99
+idade = 23
+possuiDiploma = false
