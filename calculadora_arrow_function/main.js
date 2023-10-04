8bbfe217ed9e95ff68b8f81bed7d8c0e3fec5579
eloycarneiro <40106015+eloycarneiro@users.noreply.github.com>
2023-10-04 08:16:51 -0300

    Add files via upload

---
+++ calculadora_arrow_function/main.js
@@ -0,0 +1,50 @@
+
+function realizarOperacao(){
+    const operacoes = document.getElementById("operacao")
+    const valor1 = document.getElementById("valor1")
+    const valor2 = document.getElementById("valor2")
+    let opEscolhida = operacoes.options[operacoes.selectedIndex].text
+    
+    switch (opEscolhida) {
+        case '+':
+            soma(valor1.value, valor2.value)
+            break
+        case '-':
+            subtrair(valor1.value, valor2.value)
+            break
+        case '*':
+            multiplicar(valor1.value, valor2.value)
+            break
+        case '/':
+            dividir(valor1.value, valor2.value)
+            break
+        case '%':
+            sobrar(valor1.value, valor2.value)
+            break
+        default:
+            break;
+    }
+
+}
+
+const soma = (val1, val2) => {
+    let soma = resultado.innerHTML = Number(val1) + Number(val2)
+    return soma
+}
+
+function subtrair(val1, val2) {
+    let diferenca = resultado.innerHTML = Number(val1) - Number(val2)
+    return diferenca
+}
+
+function multiplicar(val1, val2) {
+    let produto = resultado.innerHTML = Number(val1) * Number(val2)
+}
+
+function dividir(val1, val2) {
+    let quociente = resultado.innerHTML = Number(val1) / Number(val2) 
+}
+
+function sobrar(val1, val2) {
+    let resto = resultado.innerHTML = Number(val1) % Number(val2) 
+}
